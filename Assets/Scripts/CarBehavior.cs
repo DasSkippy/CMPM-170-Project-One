ae87ddd45add3826e33750b1d5bb07992698af61
DasSkippy <sean.frederico105@gmail.com>
2026-04-06 17:19:43 -0700

    worked on it

--- Assets/Scripts/CarBehavior.cs
+++ Assets/Scripts/CarBehavior.cs
@@ -4,6 +4,8 @@ public class CarBehavior : MonoBehaviour
 {
     private Rigidbody myRb;
     public float speed;
+    public float turnSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +15,33 @@ public class CarBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("drive forward");
-            myRb.linearVelocity = transform.forward * speed;
+            Drive();
+        }
+        Turn();
+    }
+
+    private void Drive()
+    {
+        Debug.Log("drive forward");
+        myRb.linearVelocity = transform.forward * speed;
+    }
+
+    private void Turn()
+    {
+        //Turn Left
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Turn Left");
+            myRb.angularVelocity = transform.forward * turnSpeed;
+        }
+
+        //Turn Right
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Turn Right");
+            myRb.angularVelocity = -transform.forward * turnSpeed;
         }
     }
 }
