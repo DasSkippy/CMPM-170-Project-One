d130a7dd9d0241aba202435741aa713cdc05c0f2
DasSkippy <sean.frederico105@gmail.com>
2026-04-11 13:59:24 -0700

    updated skyboxes

--- Assets/Scripts/SunCycle.cs
+++ Assets/Scripts/SunCycle.cs
@@ -1,11 +1,37 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SunCycle : MonoBehaviour
 {
     public float rotationSpeed = 50f;
 
+    public Material daySkybox;
+    public Material nightSkybox;
+
     void Update()
     {
+        // Rotate the sun (your original behavior)
         transform.Rotate(rotationSpeed * Time.deltaTime, 0f, 0f);
+
+        // Get current X rotation (0–360)
+        float xRotation = transform.eulerAngles.x;
+
+        // 🌅 Day when sun is above horizon
+        if (xRotation > 0f && xRotation < 180f)
+        {
+            if (RenderSettings.skybox != daySkybox)
+            {
+                RenderSettings.skybox = daySkybox;
+                DynamicGI.UpdateEnvironment();
+            }
+        }
+        // 🌙 Night when sun is below horizon
+        else
+        {
+            if (RenderSettings.skybox != nightSkybox)
+            {
+                RenderSettings.skybox = nightSkybox;
+                DynamicGI.UpdateEnvironment();
+            }
+        }
     }
 }
