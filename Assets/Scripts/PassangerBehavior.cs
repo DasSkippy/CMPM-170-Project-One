c5e51daf671ff80a768c7a7fb26dce6e17a3fda4
DasSkippy <sean.frederico105@gmail.com>
2026-04-12 19:20:43 -0700

    added passenger model

--- Assets/Scripts/PassangerBehavior.cs
+++ Assets/Scripts/PassangerBehavior.cs
@@ -15,11 +15,30 @@ public class PassangerBehavior : MonoBehaviour
     public List<PassengerInfo> passengerID = new List<PassengerInfo>();
     private PassengerInfo currentID;
     private CarBehavior carBehavior;
+    private Transform car;
 
     public void Start()
     {
         currentID = passengerID[Random.Range(0, passengerID.Count)];
         carBehavior = FindFirstObjectByType<CarBehavior>();
+        car = GameObject.Find("Truck").transform;
+    }
+
+    public void Update()
+    {
+        FaceCar();
+    }
+
+    private void FaceCar()
+    {
+        Vector3 direction = car.position - transform.position;
+        direction.y = 0f;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = rotation;
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
