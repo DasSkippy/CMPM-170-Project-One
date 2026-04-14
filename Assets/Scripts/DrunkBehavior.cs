0a6d82df5442e95291b24ee4164c81a166a3f458
DasSkippy <sean.frederico105@gmail.com>
2026-04-14 15:08:43 -0700

    added blink

--- Assets/Scripts/DrunkBehavior.cs
+++ Assets/Scripts/DrunkBehavior.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class DrunkBehavior : MonoBehaviour
 {
+    public Animator eyesAnimator;
+    public float minBlinkTime;
+    public float maxBlinkTime;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(Blink(Random.Range(minBlinkTime, maxBlinkTime)));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Blink(float time)
     {
-        
+        yield return new WaitForSeconds(time);
+        eyesAnimator.SetTrigger("blink");
+        StartCoroutine(Blink(Random.Range(minBlinkTime, maxBlinkTime)));
     }
 }
