0eb59f3f94c0d3956346badbd52f63f51c6da2cb
DasSkippy <sean.frederico105@gmail.com>
2026-04-17 16:52:23 -0700

    added credits

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ using static Unity.Collections.AllocatorManager;
 public class MainMenu : MonoBehaviour
 {
     public AudioClip click;
+    public GameObject menu, credits;
     public void StartGame()
     {
         SceneManager.LoadScene("CityScene");
@@ -14,11 +15,20 @@ public class MainMenu : MonoBehaviour
     public void Credits()
     {
         GameObject.Find("Main Camera").GetComponent<AudioSource>().PlayOneShot(click);
+        menu.SetActive(false);
+        credits.SetActive(true);
     }
 
     public void QuitGame()
     {
         Application.Quit();
         GameObject.Find("Main Camera").GetComponent<AudioSource>().PlayOneShot(click);
+    }
+
+    public void Back()
+    {
+        GameObject.Find("Main Camera").GetComponent<AudioSource>().PlayOneShot(click);
+        menu.SetActive(true);
+        credits.SetActive(false);
     }
 }
